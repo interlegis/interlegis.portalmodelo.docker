88d2e978c623867ef22438143ba2423f5bc74689
Fabio Rauber <fabiorauber@gmail.com>
2018-11-12 15:48:33 -0200

    Fixed issue with configuring with no zeo server

--- configure.sh
+++ configure.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir -p /data/blobstorage /data/filestorage /data/instance /data/log /data/zeoserver
-
 EMAIL=${EMAIL:-"root@localhost"}
 PASSWORD=${PASSWORD:-"altereme"}
 TITLE=${TITLE:-"Portal Modelo"}
@@ -20,8 +18,22 @@ sed -i "s/%SMTP_SERVER%/${SMTP_SERVER}/g" cfg_portal.py
 sed -i "s/%SMTP_PORT%/${SMTP_PORT}/g" cfg_portal.py
 sed -i "s/%ROOTPWD%/${ROOTPWD}/g" cfg_portal.py
 sed -i "s/%HOSTNAME%/${HOSTNAME}/g" cfg_portal.py
-sed -i "s/%ZEO_ADDRESS%/${ZEO_ADDRESS}/g" configure.cfg
-sed -i "s/%ZEO_ADDRESS%/${ZEO_ADDRESS}/g" upgrades.cfg
+
+if [ "$ZEO_ADDRESS" == "" ]; then
+  sed -i "s/%ZEO_ADDRESS%//g" configure.cfg
+  sed -i "s/%ZEO_ADDRESS%//g" upgrades.cfg
+  sed -i "s/%ZEO_CLIENT%/false/g" configure.cfg
+  sed -i "s/%ZEO_CLIENT%/false/g" upgrades.cfg
+  sed -i "s/%SHARED_BLOB%//g" configure.cfg
+  sed -i "s/%SHARED_BLOB%//g" upgrades.cfg
+else
+  sed -i "s/%ZEO_ADDRESS%/${ZEO_ADDRESS}/g" configure.cfg
+  sed -i "s/%ZEO_ADDRESS%/${ZEO_ADDRESS}/g" upgrades.cfg
+  sed -i "s/%ZEO_CLIENT%/true/g" configure.cfg
+  sed -i "s/%ZEO_CLIENT%/true/g" upgrades.cfg
+  sed -i "s/%SHARED_BLOB%/on/g" configure.cfg
+  sed -i "s/%SHARED_BLOB%/on/g" upgrades.cfg
+fi
 
 if [ ! -e "/data/.custom-installed.cfg" ]; then
   bin/buildout -c configure.cfg && echo "Initial configuration completed."
